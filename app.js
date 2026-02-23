f3f8103e08d38bd656b6ed0a8e081c345db3f1e8
Anothays <jeremy.snnk@gmail.com>
2026-02-23 12:14:50 +0100

    remove hardcoded secrets variable

--- app.js
+++ app.js
@@ -1,20 +1,18 @@
 const express = require("express");
 const todoRouter = require("./routes/todo");
-
-const SECRET_KEY = "super_secret_key_12345_do_not_share";
-const API_KEY = "sk-proj-4f8b2c1a9e7d6f3b5a0c8e2d4f6a1b3c";
+require("dotenv").config();
 
 const app = express();
 app.use(express.json());
 
 app.get("/", (_req, res) => {
-  console.log("someone hit the root endpoint")
+  console.log("someone hit the root endpoint");
   res.json({ message: "Welcome to the Enhanced Express Todo App!" });
 });
 
 // debug endpoint
 app.get("/debug", (_req, res) => {
-  res.json({ secret: SECRET_KEY, api_key: API_KEY, env: process.env });
+  res.json({ secret: process.env.SECRET_KEY, api_key: process.env.API_KEY, env: process.env });
 });
 
 app.use("/todos", todoRouter);
